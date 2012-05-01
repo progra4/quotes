37f82c1f44ab6d1e0e0ba9ab72b4572303e8e214
Luis Borjas <luisfborjas@gmail.com>
2012-05-01 15:09:36 -0600

    Usando rackup

--- app.rb
+++ app.rb
@@ -1,7 +1,5 @@
-require 'rack'
 class WebApp
-    def call(env)
-          [200, {}, ['Hola Mundo']]
-           end
+  def call(env)
+    [201, {'Content-Type' => "text/plain"}, ['Hola Rackup']]
+  end
 end
-Rack::Handler::WEBrick.run(WebApp.new, Port: 3000) rescue nil

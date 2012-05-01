e6e10495c6bc19c777199d39706862ad24bcf147
Luis Borjas <luisfborjas@gmail.com>
2012-05-01 16:18:20 -0600

    explorando el entorno

--- app.rb
+++ app.rb
@@ -1,5 +1,14 @@
 class WebApp
   def call(env)
-    [201, {'Content-Type' => "text/plain"}, ['Hola Rackup']]
+    lang = env['HTTP_ACCEPT_LANGUAGE'] || 'en'
+    path = env['PATH_INFO']
+
+    body = if lang == 'en'
+             "you just asked for #{path}, \n with the env #{env.to_s}"
+           else
+             "acabas de pedir #{path}, \n con el entorno #{env.to_s}"
+           end
+
+    [201, {'Content-Type' => "text/plain"}, [body]]
   end
 end

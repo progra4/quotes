3bb8f961e8607e2b5ba0604517a4b8584ce53a14
Luis Borjas <luisfborjas@gmail.com>
2012-05-02 00:59:54 -0600

    obteniendo o la colección o un miembro

--- app.rb
+++ app.rb
@@ -2,9 +2,26 @@ require './models'
 class WebApp
   def call(env)
     method = env["REQUEST_METHOD"]
+    path   = env["REQUEST_PATH"]
+
+    collection_pattern = /\/quotes$/
+    member_pattern     = /\/quotes\/([a-z0-9\-]+)/
+
     status, body = case method
       when "GET"
-        [200, Models::Quote.all.map(&:as_text).join("\n")]
+        if path =~ collection_pattern
+          [200, Models::Quote.all.map(&:as_text).join("\n")]
+        elsif path =~ member_pattern
+          id = path.match(member_pattern)[1]
+          quote = Models::Quote.find(id)
+          if quote
+            [200, quote.as_text]
+          else
+            [404, ""]
+          end
+        else
+          [501, "Not Implemented"]
+        end
       else
         [405, ""]
       end
